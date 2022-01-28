7e63932c4078c25fe23c03194e9c3e705357a6c0
Olteanu Costin <costinolt@gmail.com>
2022-01-28 21:29:08 +0200

    Lint

--- src/costin/autocomplete.clj
+++ src/costin/autocomplete.clj
@@ -1,19 +1,21 @@
 (ns costin.autocomplete
   (:gen-class)
   (:require
-    [clojure.spec.alpha :as spec]
-    [clojure.string :as string]))
+    [clojure.spec.alpha :as spec]))
 
-(defn completes? [input full-string]
-   (let [last-matched (reduce
+
+(defn completes?
+  [input full-string]
+  (let [last-matched (reduce
                        (fn [input-idx next-char]
                          (cond
                            (= input-idx (count input)) input-idx
                            (= next-char (nth input input-idx)) (inc input-idx)
-                           :not-matched input-idx))
+                           :else input-idx))
                        0 (seq full-string))]
-     (= last-matched (count input))))
+    (= last-matched (count input))))
+
 
 (spec/fdef completes?
-  :args #((and ((comp string? first) %) ((comp string? second) %)))
-  :ret boolean?)
+           :args #((and ((comp string? first) %) ((comp string? second) %)))
+           :ret boolean?)
